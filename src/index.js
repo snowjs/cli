a9ab8f4288d903cc860ec005f3da26456bc0e1da
Peter Mikitsh <peter.mikitsh@gmail.com>
2018-11-12 11:24:39 -0800

    'secrets', 'ls' subcommands

---
+++ src/index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const childProcess = require('child_process');
+const util = require('util');
+const chalk = require('chalk');
+const mri = require('mri');
+
+const exec = util.promisify(childProcess.exec);
+
+async function run(str) {
+  try {
+    const {stdout} = await exec(str);
+    console.log(stdout);
+  } catch (error) {
+    console.log(chalk.red(error.stderr));
+  }
+}
+
+function isRemove(str) {
+  return str === 'rm' || str === 'remove';
+}
+
+async function main() {
+  const {_} = mri(process.argv.slice(2));
+  const [subcommand, ...rest] = _;
+
+  if (subcommand === 'ls') {
+    const [deployment] = rest;
+    await run(`kubectl get deployments ${deployment}`);
+  }
+  if (subcommand === 'secrets') {
+    const [action, key, value] = rest;
+    if (action === 'ls') {
+      await run('kubectl get secrets');
+    }
+    if (action === 'add') {
+      await run(`kubectl create secret generic ${key} --from-literal=${key}=${value}`);
+    }
+    if (isRemove(action)) {
+      await run(`kubectl delete secret ${key}`);
+    }
+  }
+}
+
+main();
