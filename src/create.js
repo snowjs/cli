9b8d939a157d44dc636d87bc611ab282b2652b13
Peter Mikitsh <peter.mikitsh@gmail.com>
2018-12-29 14:51:48 -0800

    [create]: init helm/tiller with TLS

--- src/create.js
+++ src/create.js
@@ -62,6 +62,14 @@ module.exports = async function () {
 
       const {stdout: projectId} = await run('gcloud config get-value project');
       await run('gcloud services enable container.googleapis.com');
+
+      // Check if we have an existing cluster
+      let clusterExists = false;
+      const {stdout: clusterData} = await run('gcloud container clusters list');
+      if (clusterData.indexOf('snow-cluster') > -1) {
+        clusterExists = true;
+      }
+
       const createClusterCmd = `
         gcloud beta container \
           clusters create "snow-cluster" \
@@ -85,7 +93,121 @@ module.exports = async function () {
           --enable-autorepair \
           --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/trace.append" \
       `;
-      await run(createClusterCmd);
+
+      if (!clusterExists) {
+        await run(createClusterCmd);
+      }
+
+      // Install helm with TLS.
+      // https://medium.com/google-cloud/install-secure-helm-in-gke-254d520061f7
+
+      const BYTES = 2048;
+
+      // Create Certificate Authority
+      await run(`openssl genrsa -out $(helm home)/ca.key.pem ${BYTES}`);
+      const config = `
+        [req]\\n
+        req_extensions=v3_ca\\n
+        distinguished_name=req_distinguished_name\\n
+        [req_distinguished_name]\\n
+        [ v3_ca ]\\n
+        basicConstraints=critical,CA:TRUE\\n
+        subjectKeyIdentifier=hash\\n
+        authorityKeyIdentifier=keyid:always,issuer:always`;
+      await run(`
+        openssl req \\
+          -config <(printf '${config}') \\
+          -key $(helm home)/ca.key.pem \\
+          -new \\
+          -x509 \\
+          -days 7300 \\
+          -sha256 \\
+          -out $(helm home)/ca.cert.pem \\
+          -extensions v3_ca \\
+          -subj "/C=US"`, {shell: '/bin/bash'});
+
+      // Create credentials for tiller (server)
+      await run(`openssl genrsa -out $(helm home)/tiller.key.pem ${BYTES}`);
+      await run(`
+        openssl req \
+          -new \
+          -sha256 \
+          -key $(helm home)/tiller.key.pem \
+          -out $(helm home)/tiller.csr.pem \
+          -subj "/C=US/O=Snow/CN=tiller-server"`,
+      {shell: '/bin/bash'});
+      const tillerConfig = `
+        [SAN]\\n
+        subjectAltName=IP:127.0.0.1`;
+      await run(`
+        openssl x509 -req -days 365 \
+          -CA $(helm home)/ca.cert.pem \
+          -CAkey $(helm home)/ca.key.pem \
+          -CAcreateserial \
+          -in $(helm home)/tiller.csr.pem \
+          -out $(helm home)/tiller.cert.pem \
+          -extfile <(printf '${tillerConfig}') \
+          -extensions SAN`,
+      {shell: '/bin/bash'});
+
+      // Create credentials for helm (client)
+      await run(`openssl genrsa -out $(helm home)/helm.key.pem ${BYTES}`);
+      await run(`
+        openssl req -new -sha256 \
+          -key $(helm home)/helm.key.pem \
+          -out $(helm home)/helm.csr.pem \
+          -subj "/C=US"
+      `);
+      await run(`
+        openssl x509 -req -days 365 \
+          -CA $(helm home)/ca.cert.pem \
+          -CAkey $(helm home)/ca.key.pem \
+          -CAcreateserial \
+          -in $(helm home)/helm.csr.pem \
+          -out $(helm home)/helm.cert.pem
+      `);
+
+      // Create service account and clusterrolebinding
+      await run(`kubectl create serviceaccount \
+        --namespace kube-system tiller
+      `);
+      await run(`kubectl create clusterrolebinding \
+        tiller-cluster-rule \
+        --clusterrole=cluster-admin \
+        --serviceaccount=kube-system:tiller
+      `);
+
+      // Install Helm w/ 'tiller' service account
+      await run(`
+        helm init \
+          --debug \
+          --tiller-tls \
+          --tiller-tls-cert $(helm home)/tiller.cert.pem \
+          --tiller-tls-key $(helm home)/tiller.key.pem \
+          --tiller-tls-verify \
+          --tls-ca-cert $(helm home)/ca.cert.pem \
+          --service-account tiller \
+          --wait
+      `);
+
+      // Verify helm installation
+      await run(`
+        helm ls --tls \
+          --tls-ca-cert $(helm home)/ca.cert.pem \
+          --tls-cert $(helm home)/helm.cert.pem \
+          --tls-key $(helm home)/helm.key.pem
+      `);
+
+      /**
+       *
+       * To remove helm:
+       *
+       * helm reset
+       *   --tls
+       *   --tls-ca-cert $(helm home)/ca.cert.pem
+       *   --tls-cert $(helm home)/helm.cert.pem
+       *   --tls-key $(helm home)/helm.key.pem
+       */
       break;
     }
     default: {
