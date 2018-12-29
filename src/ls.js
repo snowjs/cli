79a59cc3c625b245368dd615c71b01526e5209e0
Peter Mikitsh <peter.mikitsh@gmail.com>
2018-12-28 21:05:34 -0800

    [ls] fix base case (no deployment name specified)

--- src/ls.js
+++ src/ls.js
@@ -2,5 +2,8 @@ const {run} = require('./utils');
 
 module.exports = async function (args) {
   const [deployment] = args;
+  if (!deployment) {
+    return run('kubectl get deployments');
+  }
   await run(`kubectl get deployments ${deployment}`);
 };
