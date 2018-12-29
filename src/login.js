5df520fd171da1eaa8f9191abb78086bb4afdaef
Peter Mikitsh <peter.mikitsh@gmail.com>
2018-12-28 20:56:31 -0800

    [login] implementation

---
+++ src/login.js
@@ -0,0 +1,23 @@
+const {logError, pickOne, run} = require('./utils');
+
+module.exports = async function () {
+  const cloudProviders = ['minikube', 'gcp'];
+  const question = 'Which cloud provider do you want to login to?';
+  const provider = await pickOne(question, cloudProviders);
+  switch (provider) {
+    case 'minikube': {
+      await run('kubectl config use-context minikube');
+      break;
+    }
+    case 'gcp': {
+      await run('gcloud auth login');
+      const {stdout: clusterData} = await run('gcloud container clusters list --format="json"');
+      const {location, name} = JSON.parse(clusterData)[0];
+      await run(`gcloud container clusters get-credentials ${name} --zone "${location}"`);
+      break;
+    }
+    default: {
+      logError('No valid cloud provider selected.');
+    }
+  }
+};
